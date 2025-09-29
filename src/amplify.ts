2feb0c8f2d3a1c33f06d35e68b58dac5ff7ba335
Daniel Jankowski <djank0113@gmail.com>
2025-09-29 15:41:58 -0400

    feat: add Amplify ESLint rule overrides and update canonical rules
    - Add import/no-extraneous-dependencies and canonical/filename-match-regex overrides for Amplify config
    - Update no-else-return and no-unreachable from numeric to string 'warn' format in canonical config

--- src/amplify.ts
+++ src/amplify.ts
@@ -12,6 +12,8 @@ export const amplifyConfig: FlatConfigArray = [
       '@typescript-eslint/await-thenable': 'off',
       'import/no-unresolved': 'warn',
       'no-console': 'off',
+      'import/no-extraneous-dependencies': 'off',
+      'canonical/filename-match-regex': 'off',
     },
     files: ['amplify/**/*.{js,ts}'],
   },
